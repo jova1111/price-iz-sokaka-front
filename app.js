e94d2837075bf1d579893bd5683c183c2af3c61d
Jovan Dragutinovic <jocandragutinovic@gmail.com>
2018-03-17 21:23:14 +0100

    Added redirecting all pages to index.html.

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname+'/dist'));
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
